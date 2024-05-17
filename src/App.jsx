43ee765d9cb903a9dea21ebeeb9c162e597b4718
Colin Beltz <colinbeltz@gmail.com>
2024-05-17 14:40:02 -0500

    Nike2

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { Nav, Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer } from './sections';
+
+const App = () => (
+    <main className="relative">
+      
+      <section className="xl:padding-1 wide: padding-r padding-b">
+        <Nav />
+      </section>
+
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+
+      <section className="padding">
+        <PopularProducts />
+      </section>
+
+      <section className="padding">
+        <SuperQuality />
+      </section>
+
+      <section className="padding padding-x py-10">
+        <Services />
+      </section>
+
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+
+      <section className="bg-black padding-x paddying-t pb-8">
+        <Footer />
+      </section>
+
+    </main>
+  )
+  export default App
