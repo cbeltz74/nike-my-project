5d402ac7443571e27a04d53fa1abfa2ae9e54b8d
Colin Beltz <colinbeltz@gmail.com>
2024-05-20 22:45:18 -0500

    Subscribe section done. Button component still not rendering correctly.

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,6 +1,20 @@
+// import {Button} from '../components/Button'
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
+      <h3 className="text-3xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up for
+      <span className="text-coral-red"> Updates </span> & Newsletter
+      </h3>
+      <div className="lg:max-w-[40%] w-full flex item-center max-sm:flex-col p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+      </div>
+
+    <div className="flex max-sm:justify-end items-center max-sm:w-full">
+            {/* <Button label="Sign" fullWidth /> */}
+
+    </div>
+    </section>
   )
 }
 
