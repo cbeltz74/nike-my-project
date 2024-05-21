f9c0ed7e0c07214ee194550ebeddd173c3c894e0
Colin Beltz <colinbeltz@gmail.com>
2024-05-20 21:07:01 -0500

    Completed Reviews section and ReviewCard component.

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -4,9 +4,9 @@ import ServiceCard from "../components/ServiceCard"
 const Services = () => {
   return (
     <section className="max-container flex justify-center flex-wrap gap-9">
-        {services.map((service) => (
+        {services.map((service, index) => (
           <ServiceCard
-          key={service.name} {...service} />
+          key={index} {...service} />
         ))}
     </section>
   )
