53feced657edce3cde9f9d6ea7658a73ae326f77
Colin Beltz <colinbeltz@gmail.com>
2024-05-21 18:39:23 -0500

    Finished footer

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,11 +1,12 @@
-// import { arrowRight } from "../assets/icons";
-// import { offer } from "../assets/images";
-// import { Button } from "../components";
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import { Button } from "../components/Button";
+import { LearnButton } from '../components/LearnButton'
 
 const SpecialOffer = () => {
   return (
     <section className='flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container'>
-      {/* <div className='flex-1'>
+      <div className='flex-1'>
         <img
           src={offer}
           alt='Shoe Promotion'
@@ -13,7 +14,7 @@ const SpecialOffer = () => {
           height={687}
           className='object-contain w-full'
         />
-      </div> */}
+      </div>
       <div className='flex flex-1 flex-col'>
         <h2 className='text-4xl font-palanquin font-bold'>
           <span className='text-coral-red'>Special </span>
@@ -29,15 +30,10 @@ const SpecialOffer = () => {
           desires, surpassing the loftiest expectations. Your journey with us is
           nothing short of exceptional.
         </p>
-        <div className='mt-11 flex flex-wrap gap-4'>
-          {/* <Button label='Shop now' iconURL={arrowRight} />
-          <Button
-            label='Learn more'
-            backgroundColor='bg-white'
-            borderColor='border-slate-gray'
-            textColor='text-slate-gray'
-          /> */}
-        </div>
+          <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label="Shop now" iconURL={arrowRight}/>
+          <LearnButton label="Learn more" />
+          </div>
       </div>
     </section>
   );
